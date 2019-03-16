fcac88b67fc133a0721921d9e5a1c88757afab95
mpartee123 <m_partee@hotmail.com>
2019-03-16 16:30:44 -0500

    fixed cli ui for prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,45 +12,42 @@ connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     queryAllProducts();
-    start();
 });
 
 function queryAllProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             productId.push(res[i].item_id);
-            console.log(`
-            Id: ${res[i].item_id}
-            Item Name:${res[i].product_name}
-            Price:${res[i].price}
-            `);
+            console.log(`Id: ${res[i].item_id}, Item Name:${res[i].product_name}, Price:${res[i].price}`);
         }
+        start();
     });
+
 }
 
 
 // 6. The app should then prompt users with two messages.
 // * The first should ask them the ID of the product they would like to buy.
 // * The second message should ask how many units of the product they would like to buy.
-const inquirer = require('inquirer');
+
 function start() {
-    inquirer
-        .prompt({
-            name: "idRequest",
-            type: "input",
-            message: "Which product ID would you like to buy?"
-        })
-        .then(function(answer) {
-            // based on their answer, either call the bid or the post functions
-            if (answer.idRequest === productId) {
-                console.log('user input'+answer.idRequest);
+    const inquirer = require('inquirer');
+    const questions=[
+        {
+                name: "idRequest",
+                type: "input",
+                message: "Which product ID would you like to buy?"
+            },
+            {
+                name:"idQuantity",
+                type: "input",
+                message: "How many would you like to purchase?"
             }
-            // else if(answer.postOrBid === "BID") {
-            //     bidAuction();}
-                else{
-                connection.end();
-            }
+    ];
+    inquirer.prompt(questions).then(answer => {
+         console.log('Id request answer '+answer.idRequest);
+         console.log('quantity answer '+answer.idQuantity);
+
         });
-    console.log('product Id array after input'+productId);
 }
 
