e22b8939df0ce93f18b255ea261d0a4ab0f4c8c8
mpartee123 <m_partee@hotmail.com>
2019-03-16 19:17:51 -0500

    Finished CL UI

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,49 +5,113 @@ const connection = mysql.createConnection({
     user: 'root',
     database: "bamazon",
 });
-let productId=[];
+
 
 // create the connection information for the sql database
 connection.connect(function (err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
     queryAllProducts();
 });
 
+//Shows available inventory to user
 function queryAllProducts() {
+    let productId = [];
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
             productId.push(res[i].item_id);
-            console.log(`Id: ${res[i].item_id}, Item Name:${res[i].product_name}, Price:${res[i].price}`);
+            console.log(`Id: ${res[i].item_id}, Item Name: ${res[i].product_name}, Price: ${res[i].price}`);
         }
         start();
     });
-
 }
 
-
-// 6. The app should then prompt users with two messages.
-// * The first should ask them the ID of the product they would like to buy.
-// * The second message should ask how many units of the product they would like to buy.
-
+//Asks for user input to place order
 function start() {
     const inquirer = require('inquirer');
-    const questions=[
+    const questions = [
         {
-                name: "idRequest",
-                type: "input",
-                message: "Which product ID would you like to buy?"
+            name: "idRequest",
+            type: "input",
+            message: "Which product ID would you like to buy?"
+        },
+        {
+            name: "idQuantity",
+            type: "input",
+            message: "How many would you like to purchase?"
+        }
+    ];
+
+    inquirer.prompt(questions).then(answer => {
+        checkQuantity(answer.idRequest, answer.idQuantity);
+    });
+}
+
+//Checks inventory and processes request if stock is available
+function checkQuantity(id, userReqQty) {
+    connection.query('SELECT * FROM products WHERE item_id =' + id, function (error, results) {
+        var res = results[0];
+        if (error) throw error;
+        if (userReqQty > res.stock_quantity || userReqQty < 0) {
+            console.log(`We're so sorry but there are ${res.stock_quantity} ${res.product_name} available please revise your quantity...`);
+            queryAllProducts();
+        } else {
+
+            var newQuantity = res.stock_quantity - userReqQty;
+            updateInventory(id, userReqQty, newQuantity, res.price, res.product_name);
+        }
+    });
+}
+
+//Updates inventory if inventory is available for purchase
+function updateInventory(id, userReqQty, newQuantity, price, name) {
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: newQuantity
             },
             {
-                name:"idQuantity",
-                type: "input",
-                message: "How many would you like to purchase?"
+                item_id: id
             }
-    ];
-    inquirer.prompt(questions).then(answer => {
-         console.log('Id request answer '+answer.idRequest);
-         console.log('quantity answer '+answer.idQuantity);
 
+        ],
+        function (error) {
+            if (error) throw err;
+            console.log("Item " + name + " has been successfully updated!");
+            console.log(`
+                    Thank your for your purchase!
+                    Your total for ${name} is ${price * userReqQty}
+            `);
+            newStockQty(id);
         });
 }
 
+//Displays stock for item to show that the purchase was processed
+function newStockQty(id) {
+    connection.query('SELECT * FROM products WHERE item_id =' + id, function (error, results) {
+        var res = results[0];
+        if (error) throw error;
+        console.log(`The quantity for ${res.product_name} is now ${res.stock_quantity}`);
+        restart()
+    });
+}
+
+//Restarts prompts if user would like to continue
+function restart() {
+    let inquirer = require('inquirer');
+
+    inquirer
+        .prompt([
+            {
+                type:'list',
+                name:'exit',
+                choices:['Yes','NO']
+            }])
+        .then(answers => {
+            if(answers.exit==='Yes'){
+                queryAllProducts();
+            }else {
+                connection.end();
+            }
+        })
+}
